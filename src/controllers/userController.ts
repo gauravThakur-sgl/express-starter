48ee7368ee2d8f5e5fbfe371f9ab9df0cc2e5fd6
Shubham <shubham.jha@shipglobal.in>
2025-01-13 19:40:25 +0530

    Added response in login

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,86 +3,100 @@ import { User } from "../models/user";
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 import {
- signup as signupService,
- login as loginService,
- logout as logoutService,
- checkAuth as checkAuthService,
+  signup as signupService,
+  login as loginService,
+  logout as logoutService,
+  checkAuth as checkAuthService,
 } from "../services/user.service";
 
 interface SignupRequest extends Request {
- body: {
-  email: string;
-  password: string;
-  firstName: string;
-  middleName: string;
-  lastName: string;
-  dateOfBirth: string;
-  bio: string;
-  links: string[];
-  font: string;
-  theme: string;
-  activeModules: string[];
- };
+  body: {
+    email: string;
+    password: string;
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    dateOfBirth: string;
+    bio: string;
+    links: string[];
+    font: string;
+    theme: string;
+    activeModules: string[];
+  };
 }
 
 export const signup = async (
- req: SignupRequest,
- res: Response,
- next: NextFunction
+  req: SignupRequest,
+  res: Response,
+  next: NextFunction
 ) => {
- try {
-  const newUser = await signupService(req.body);
-  res.status(200).json(newUser);
- } catch (error: any) {
-  console.error(`Error while saving user`, error.message);
-  next(error);
- }
+  try {
+    const newUser = await signupService(req.body);
+    res.status(200).json(newUser);
+  } catch (error: any) {
+    console.error(`Error while saving user`, error.message);
+    next(error);
+  }
 };
 
 // Login
 interface LoginRequest extends Request {
- body: {
-  email: string;
-  password: string;
- };
+  body: {
+    email: string;
+    password: string;
+  };
 }
 
 export const login = async (req: LoginRequest, res: Response) => {
- try {
-  const { email, password } = req.body;
-  const { user, token, exp } = await loginService(email, password);
-  // Setting the Cookie
-  res.cookie("token", token, {
-   expires: new Date(exp * 1000),
-   httpOnly: true,
-   sameSite: "lax",
-   secure: process.env.Node_ENV === "production",
-  });
+  try {
+    const { email, password } = req.body;
+    const { user, token, exp } = await loginService(email, password);
+    // Setting the Cookie
+    res.cookie("token", token, {
+      expires: new Date(exp * 1000),
+      httpOnly: true,
+      sameSite: "lax",
+      secure: process.env.Node_ENV === "production",
+    });
 
-  res.status(200).json({ message: "User Successfully logged in" });
- } catch (error) {
-  console.log(error);
-  res.status(401).json({ message: "login failed" });
- }
+    res.status(200).json({
+      message: "User Successfully logged in",
+      data: {
+        email: user.email,
+        firstName: user.firstName,
+        middleName: user.middleName,
+        lastName: user.lastName,
+        dateOfBirth: user.dateOfBirth,
+        bio: user.bio,
+        links: user.links,
+        font: user.font,
+        theme: user.theme,
+        activeModules: user.activeModules,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ message: "login failed" });
+  }
 };
 
 export const logout = async (req: Request, res: Response) => {
- try {
-  logoutService();
-  res.clearCookie("token");
-  res.status(200).json({ message: "Logged out successfully" });
- } catch (error: any) {
-  console.log(`Error while logging out`, error.message);
-  res.status(500).json({ message: "Internal server error" });
- }
+  try {
+    logoutService();
+    res.clearCookie("token");
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error: any) {
+    console.log(`Error while logging out`, error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const checkAuth = async (req: Request, res: Response) => {
- try {
-  console.log("req.user", req.user);
-  res.status(200).json({ message: "User is authenticated" });
- } catch (error) {
-  console.log(error);
-  res.status(500).json({ message: "checkAuth failed" });
- }
+  try {
+    console.log("req.user", req.user);
+    res.status(200).json({ message: "User is authenticated" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "checkAuth failed" });
+  }
 };
